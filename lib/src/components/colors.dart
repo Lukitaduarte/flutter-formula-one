eec8b6b135855a46f90218bb0e125498e1c389d9
Lucas Duarte <lucasduartesouza@hotmail.com>
2020-08-01 21:10:18 -0300

    added new color in design system

--- lib/src/components/colors.dart
+++ lib/src/components/colors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-const FormulaPrimaryDark = Color(0xFF38383F);
+const FormulaPrimaryDark = Color(0xFF15151E);
+const FormulaDark = Color(0xFF38383F);
 const FormulaPrimary = Color(0xFFFFFFFF);
 const FormulaAccent = Color(0xFFE10600);
 const FormulaGray = const Color(0xFFF3F3F3);
